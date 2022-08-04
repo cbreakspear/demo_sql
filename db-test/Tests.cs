94db32b98cc1a979e30558ab65da79845774e3a7
Craig Breakspear <cbreakspear@hotmail.com>
2022-08-03 22:49:43 -0400

    Added fix to test

--- db-test/Tests.cs
+++ db-test/Tests.cs
@@ -16,8 +16,8 @@ namespace AzureSQLDevelopers.Database
         [OneTimeSetUp]
         public void Setup()
         {
-            DotNetEnv.Env.Load(Environment.CurrentDirectory + "/../../../" + Env.DEFAULT_ENVFILENAME);            
-            ConnectionString = Environment.GetEnvironmentVariable("ConnectionString");            
+            DotNetEnv.Env.Load(Environment.CurrentDirectory + "/" + Env.DEFAULT_ENVFILENAME);   
+            var connectionString = Environment.GetEnvironmentVariable("ConnectionString");           
         }
 
         [Test]
