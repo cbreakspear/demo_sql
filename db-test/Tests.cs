f1d730579bed8203e0156fc3b3ea0a6a6966688b
= <=>
2022-08-15 23:11:39 -0400

    Updated Test

--- db-test/Tests.cs
+++ db-test/Tests.cs
@@ -35,7 +35,7 @@ namespace AzureSQLDevelopers.Database
                     conn.Open();
                     var result = cmd.ExecuteScalar().ToString();
                     var jsonResult = result;
-                    Assert.AreEqual("16", jsonResult);
+                    Assert.AreEqual("1", jsonResult);
                 }
             
                 
